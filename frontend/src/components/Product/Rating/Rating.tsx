ee6a881b06f52bd2104bc0c12d810f75f0bfc639
Ashley <thy.ashley@gmail.com>
2020-10-10 22:08:02 +0800

    touched up reviews and product rating

--- frontend/src/components/Product/Rating/Rating.tsx
+++ frontend/src/components/Product/Rating/Rating.tsx
@@ -3,16 +3,17 @@ import React from "react";
 interface RatingProps {
   value: number;
   text?: string;
+  color?: string;
 }
 
-const Rating: React.FC<RatingProps> = ({ value, text }) => {
+const Rating: React.FC<RatingProps> = ({ value, text, color }) => {
   const mapValueToArray = (rating: number): number[] => {
     let star = [];
     for (let i = 1; i < 6; i++) {
       if (i <= rating) {
         star.push(1);
       } else {
-        if (i - 0.5 < rating) {
+        if (i - 0.5 <= rating) {
           star.push(0.5);
         } else {
           star.push(0);
@@ -27,19 +28,31 @@ const Rating: React.FC<RatingProps> = ({ value, text }) => {
       {mapValueToArray(value).map((rating: number, idx: number) => {
         if (rating === 1) {
           return (
-            <span data-testid="full-rating" key={`rating ${idx}`}>
+            <span
+              style={{ color }}
+              data-testid="full-rating"
+              key={`rating ${idx}`}
+            >
               <i className="fas fa-star" />
             </span>
           );
         } else if (rating === 0.5) {
           return (
-            <span data-testid="half-rating" key={`rating ${idx}`}>
+            <span
+              style={{ color }}
+              data-testid="half-rating"
+              key={`rating ${idx}`}
+            >
               <i className="fas fa-star-half-alt" />
             </span>
           );
         } else {
           return (
-            <span data-testid="no-rating" key={`rating ${idx}`}>
+            <span
+              style={{ color }}
+              data-testid="no-rating"
+              key={`rating ${idx}`}
+            >
               <i className="far fa-star" />
             </span>
           );
@@ -48,6 +61,10 @@ const Rating: React.FC<RatingProps> = ({ value, text }) => {
       <span data-testid="review-text">{text && text}</span>
     </div>
   );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
 };
 
 export default Rating;
